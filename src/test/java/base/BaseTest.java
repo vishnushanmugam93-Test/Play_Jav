5dce0cc3a05e03b0af5264ed839788fa2057e772
vishnushanmugam93-Test <vishnu.shanmugam93@gmail.com>
2026-03-31 13:28:40 +0530

    fix

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -38,7 +38,7 @@ public class BaseTest {
 		// Initialize Playwright and launch a Chromium browser with specified options
 		playwright = Playwright.create();
 		browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-				.setHeadless(false)
+				.setHeadless(true)
 				.setSlowMo(50)
 				.setArgs(Arrays.asList("--window-size=1920,1080"))
 				);
