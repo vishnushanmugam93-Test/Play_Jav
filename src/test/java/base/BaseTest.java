0a598464e79a20d3c6a29ff3b6c3b0812f2fa2bb
vishnushanmugam93-Test <vishnu.shanmugam93@gmail.com>
2026-04-01 13:29:34 +0530

    Set browser to launch in headless mode

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -40,7 +40,7 @@ public class BaseTest {
         extent = ExtentManager.getInstance();
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false)
+                .setHeadless(true)
                 .setSlowMo(50)
                 .setArgs(Arrays.asList("--window-size=1920,1080"))
         );
